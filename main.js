cedfa38cbe026acc8e54ea6c8ccf8326ad25baec
PatriciaRocas <patricia_rocas@yahoo.com>
2020-11-14 20:52:41 +0200

    Tema-curs1

---
+++ main.js
@@ -0,0 +1,13 @@
+var dog = {
+    name: "Bobo",
+    color: "gray",
+    height: 3,
+    kg: 5
+}
+console.log(dog);
+
+ var myName = "Patricia"
+ function name(myName){
+ return "Numele meu este "+ myName;
+ }
+console.log(name(myName));
